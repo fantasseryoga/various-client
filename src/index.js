5a8d95241106e841e8f638906c195dc6bf12f537
fantasseryoga <69531863+fantasseryoga@users.noreply.github.com>
2023-11-02 13:48:28 +0200

    change proxy

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function noop() {}
 
 const defaultState = {
     token: null,
-    server: "http://52.23.255.170:5000",
+    server: "http://54.174.202.242:5000",
     login: noop,
     logout: noop,
     isAuthenticated: false,
